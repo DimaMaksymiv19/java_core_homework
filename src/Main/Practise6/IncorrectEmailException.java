83bd790af33ac8038e0025f75141f33b3592957f
DimaMaksymiv19 <dimamaksymiv2001@gmail.com>
2021-02-26 20:24:27 +0200

    Practise 5

---
+++ src/Main/Practise6/IncorrectEmailException.java
@@ -0,0 +1,8 @@
+package Main.Practise6;
+
+public class IncorrectEmailException extends Exception{
+
+    public IncorrectEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/Main/Practise6/UnderAgeException.java
@@ -0,0 +1,8 @@
+package Main.Practise6;
+
+public class UnderAgeException extends Exception{
+
+    public UnderAgeException(String message) {
+        super(message);
+    }
+}
